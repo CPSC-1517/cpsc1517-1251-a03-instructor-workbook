f5ad3bf673df6873ada016cbfa5301957943a55e
Sam Wu <swu@nait.ca>
2025-09-15 14:40:49 -0600

    Added Unit Test examples

---
+++ CPSC1517-Demo-Projects/Module01.Lesson05.ClassLibrary.Tests/CalculatorTest.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions; // for Should() extension method
+
+namespace Module01.Lesson05.ClassLibrary.Tests
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void Add_ShouldReturnSum_WhenTwoPositiveIntegers()
+        {
+            // Arrange
+            int x = 2;
+            int y = 3;
+
+            // Act
+            int result = Calculator.Add(x, y);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+
+        [Theory]
+        [InlineData(2,3,5)]
+        [InlineData(-1,1,0)]
+        [InlineData(0,0,0)]
+        public void Add_ShowReturnExpectedSum(int x, int y, int expectedResult)
+        {
+            // Arrange and Act
+            int actualResult = Calculator.Add(x, y);
+            
+            // Assert (xUnit)
+            Assert.Equal(expectedResult, actualResult);
+
+            // Assert (FluentAssertion )
+            actualResult.Should().Be(expectedResult);
+        }
+
+        [Theory]
+        [InlineData(10, 2, 5)]
+        [InlineData(10, -5, -2)]
+        [InlineData(-100, -5, 20)]
+        public void Divide_ShouldReturnQuotient(int numerator, int denominator, int expectedResult)
+        {
+            // Arrange and Act
+            var actualResult = Calculator.Divide(numerator, denominator);
+
+            // Assert (xUnit)
+            Assert.Equal(expectedResult, actualResult);
+
+            // Assert (FluentAssertion)
+            actualResult.Should().Be(expectedResult);
+        }
+
+        [Fact]
+        public void Divide_ShowThrow_WhenDenominatorIsZero()
+        {
+            // Arrange and Act
+            Action act = () => Calculator.Divide(20,0);
+            // Assert (FluentAssertion)
+            act.Should().Throw<DivideByZeroException>()
+                .WithMessage("*zero*");
+        }
+    }
+}
---
+++ CPSC1517-Demo-Projects/Module01.Lesson05.ClassLibrary/Calculator.cs
@@ -0,0 +1,20 @@
+﻿namespace Module01.Lesson05.ClassLibrary
+{
+    public static class Calculator
+    {
+        public static int Add(int x, int y)
+        {
+
+            return x + y;
+
+        }
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        public static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
