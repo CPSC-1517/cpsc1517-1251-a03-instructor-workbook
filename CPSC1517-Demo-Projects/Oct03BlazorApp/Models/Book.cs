2d7a7fc8107e2b8916bb1ce406dbf229c1d28c96
Sam Wu <swu@nait.ca>
2025-10-10 11:26:47 -0600

    Set PublishDate to current date

--- CPSC1517-Demo-Projects/Oct03BlazorApp/Models/Book.cs
+++ CPSC1517-Demo-Projects/Oct03BlazorApp/Models/Book.cs
@@ -4,7 +4,7 @@
     {
         public string? Title { get; set; }
         public int Pages { get; set; }
-        public DateOnly PublishDate { get; set; }
+        public DateOnly PublishDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
         public string Genre { get; set; } = "Fiction";
         public bool InStock { get; set; }
     }
