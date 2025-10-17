857d60ee2f4adb53e06dea6a1a7a01bae4ee66f8
Sam Wu <swu@nait.ca>
2025-10-17 11:49:19 -0600

    Blazor Form Valildation and saving to file

---
+++ CPSC1517-Demo-Projects/BookBlazorApp/Models/BookNoDataAnnotations.cs
@@ -0,0 +1,18 @@
+﻿namespace BookBlazorApp.Models
+{
+    public class BookNoDataAnnotations
+    {
+        public string? Title { get; set; }
+        public int Pages { get; set; }
+        public DateOnly PublishDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public string Genre { get; set; } = "Fiction";
+        public bool InStock { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title},{Pages},{PublishDate},{Genre},{InStock}";
+        }
+
+        
+    }
+}
